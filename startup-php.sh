617af969f394e7b21dc7c0a96098c7a152045915
Ivan Andreyev <ewantlf@gmail.com>
2018-11-11 16:37:41 +0300

    php-fpm version update

--- startup-php.sh
+++ startup-php.sh
@@ -4,7 +4,7 @@ set -x
 
 MEM=${PHP_MEM:=256M}
 
-sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php/7.0/fpm/php.ini
+sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php/7.2/fpm/php.ini
 
 mkdir /run/php
 php-fpm7.2 --nodaemonize

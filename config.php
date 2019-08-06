a98689ab6f67e71a10c20aa4881da099bc3c26b5
Ivan Andreyev <ewantlf@gmail.com>
2019-08-06 19:24:06 +0300

    rutorrent config update

--- config.php
+++ config.php
@@ -6,6 +6,13 @@
 	@define('HTTP_TIME_OUT', 30, true);	// in seconds
 	@define('HTTP_USE_GZIP', true, true);
 	$httpIP = null;				// IP string. Or null for any.
+	$httpProxy = array
+	(
+		'use' 	=> false,
+		'proto'	=> 'http',		// 'http' or 'https'
+		'host'	=> 'PROXY_HOST_HERE',
+		'port'	=> 3128
+	);
 
 	@define('RPC_TIME_OUT', 5, true);	// in seconds
 
@@ -19,7 +26,7 @@
 	$schedule_rand = 10;			// rand for schedulers start, +0..X seconds
 
 	$do_diagnostic = true;
-	$log_file = '/tmp/errors.log';		// path to log file (comment or leave blank to disable logging)
+	$log_file = '/downloads/.rutorrent/errors.log';		// path to log file (comment or leave blank to disable logging)
 
 	$saveUploadedTorrents = true;		// Save uploaded torrents to profile/torrents directory or not
 	$overwriteUploadedTorrents = false;     // Overwrite existing uploaded torrents in profile/torrents directory or make unique name
@@ -40,11 +47,13 @@
 	$XMLRPCMountPoint = "/RPC2";		// DO NOT DELETE THIS LINE!!! DO NOT COMMENT THIS LINE!!!
 
 	$pathToExternals = array(
-		"php" 	=> '',			// Something like /usr/bin/php. If empty, will be found in PATH.
-		"curl"	=> '/usr/bin/curl',	// Something like /usr/bin/curl. If empty, will be found in PATH.
-		"gzip"	=> '',			// Something like /usr/bin/gzip. If empty, will be found in PATH.
-		"id"	=> '',			// Something like /usr/bin/id. If empty, will be found in PATH.
-		"stat"	=> '/usr/bin/stat',	// Something like /usr/bin/stat. If empty, will be found in PATH.
+          "php"   => '/usr/bin/php',      // Something like /usr/bin/php. If empty, will be found in PATH.
+          "curl"  => '/usr/bin/curl',     // Something like /usr/bin/curl. If empty, will be found in PATH.
+          "gzip"  => '/bin/gzip',         // Something like /usr/bin/gzip. If empty, will be found in PATH.
+          "id"    => '/usr/bin/id',       // Something like /usr/bin/id. If empty, will be found in PATH.
+          "stat"  => '/usr/bin/stat',     // Something like /usr/bin/stat. If empty, will be found in PATH.
+          "pgrep" => '/usr/bin/pgrep',
+          "python"=> '/usr/bin/python',
 	);
 
 	$localhosts = array( 			// list of local interfaces
@@ -58,3 +67,8 @@
 						// For example, if Webserver and rtorrent users are in the same group then the value may be 0770.
 
 	$tempDirectory = null;			// Temp directory. Absolute path with trail slash. If null, then autodetect will be used.
+
+	$canUseXSendFile = false;		// If true then use X-Sendfile feature if it exist
+
+	$locale = "UTF8";
+
